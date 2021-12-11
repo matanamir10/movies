659b7dfa730965727b81f296c8a1f76a4d803ef3
Matan Amir <matan6080@gmail.com>
2021-12-11 22:53:23 +0200

    Setup

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error id illo minima consectetur
+        maxime mollitia, nostrum, recusandae culpa nemo saepe porro, aliquid beatae! Neque
+        asperiores maxime, facere in deleniti recusandae odio dolores reiciendis fugiat, et fuga
+        consequatur, saepe dolorum! Accusamus cum laborum id sit labore vel suscipit ipsam velit
+        dolores dolore harum, laudantium eum repudiandae quaerat dolorum unde provident fugiat
+        officia eligendi omnis! Iure consequuntur mollitia accusamus nemo eaque numquam aspernatur
+        laudantium suscipit labore neque culpa quis cumque libero repellendus deleniti dolore
+        facilis, natus deserunt. Optio, dignissimos. Hic, deserunt, beatae minima iure laudantium
+        libero quasi ab ad incidunt architecto repellendus.
+      </p>
+    </div>
+  );
+};
+
+export default App;
