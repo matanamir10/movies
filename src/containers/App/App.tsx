15aa6c0d69d73cc5c557ed63ca5dc7e91c62bb16
Matan Amir <matan6080@gmail.com>
2021-12-11 23:22:33 +0200

    Added FormItem

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import FormItem from '../../components/UI/FormItem/FormItem';
 import Input from '../../components/UI/Input/Input';
 import './App.css';
 
 const App = () => {
   return (
     <div>
-      <Input />
+      <FormItem label="Email">
+        <Input placeholder="Hello world..." />
+      </FormItem>
     </div>
   );
 };
